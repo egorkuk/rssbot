9dd0cb0dd33fbb77b00dba53f78a921d28bd2edb
egorkuk <71507934+egorkuk@users.noreply.github.com>
2022-05-31 19:09:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const path = require('path')
+
 require('dotenv').config({ path: path.join(__dirname, '.env') })
 
 const { Telegraf } = require('telegraf')
 
 const { CommandHandler } = require('./src/handlers/commandHandler')
+const { CallbackHandler } = require('./src/handlers/callbackHandler')
+const { TextHandler } = require('./src/handlers/textHandler')
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 CommandHandler(bot)
+CallbackHandler(bot)
+TextHandler(bot)
+
 
 
 
 bot.launch()
-
