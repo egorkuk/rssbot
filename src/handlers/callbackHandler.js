38a2d595aabf0074112de56586c3ffce1ce3cd47
egorkuk <71507934+egorkuk@users.noreply.github.com>
2022-05-31 19:10:40 +0200

    Update callbackHandler.js

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -1,6 +1,41 @@
+const Parser = require('rss-parser')
+
+const { InlineKeyboard } = require('../buttons')
+
 module.exports.CallbackHandler = (bot) => {
     bot.on('callback_query', async (ctx) => {
         const { callbackQuery } = ctx
-        console.log(callbackQuery)
+        console.log(callbackQuery.data)
+
+        if (callbackQuery.data ==='listRss') {
+            await ctx.editMessageText('Выбери источник 👇', InlineKeyboard.buttons_source())
+        }
+
+        if (callbackQuery.data ==='redditRss') {
+            const parser = new Parser()
+
+            let feed = await parser.parseURL('https://www.reddit.com/.rss');
+            console.log(feed.title);
+
+            const newsList = feed.items.map(item => item.title)
+
+            const newsMessageText = newsList.join('\n\n')
+            await ctx.editMessageText(`Новости с Reddit:\n\n${newsMessageText}`, InlineKeyboard.buttons_source())
+  
+        }
+        if (callbackQuery.data ==='VCruRss') {
+            const parser = new Parser()
+
+            let feed = await parser.parseURL('https://vc.ru/rss/all');
+            console.log(feed.title);
+
+            const newsList = feed.items.map(item => item.title)
+
+            const newsMessageText = newsList.join('\n\n')
+            await ctx.editMessageText(`Новости с VCru:\n\n${newsMessageText}`, InlineKeyboard.buttons_source())
+  
+        }
+
+       
     })
 }
